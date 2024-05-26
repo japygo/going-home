408630b1683b3956f3d5f407706990f015ae830e
japygo <hyunnjinn@gmail.com>
2024-05-26 17:26:34 +0900

    init project

---
+++ src/main/java/com/project/goinghome/GoingHomeApplication.java
@@ -0,0 +1,13 @@
+package com.project.goinghome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoingHomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoingHomeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/goinghome/GoingHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.goinghome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoingHomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
