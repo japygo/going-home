a3453912836c9f06d607340039df8176368da2b4
japygo <hyunnjinn@gmail.com>
2024-06-03 16:18:05 +0900

    refactor: 에러 메시지 타입 변경

--- src/main/java/com/project/goinghome/common/error/ErrorHandler.java
+++ src/main/java/com/project/goinghome/common/error/ErrorHandler.java
@@ -1,23 +1,34 @@
 package com.project.goinghome.common.error;
 
+import com.project.goinghome.common.error.exception.BadRequestException;
 import com.project.goinghome.common.error.exception.InternalServerErrorException;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.coyote.BadRequestException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @RestControllerAdvice
-public class ErrorHandler {
+public class ErrorHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler({
             BadRequestException.class,
-            IllegalArgumentException.class,
+            IllegalArgumentException.class
     })
-    public ResponseEntity<ErrorResponse> badRequest(BadRequestException e) {
+    public ResponseEntity<ErrorResponse> badRequest(Exception e) {
         log.error("BadRequestException : {}", e.getMessage(), e);
+
         return ResponseEntity.badRequest()
                 .body(ErrorResponse.of(HttpStatus.BAD_REQUEST, e.getMessage()));
     }
@@ -28,7 +39,25 @@ public class ErrorHandler {
     })
     public ResponseEntity<ErrorResponse> exception(Exception e) {
         log.error("그 외 Exception : {}", e.getMessage(), e);
+
         return ResponseEntity.internalServerError()
                 .body(ErrorResponse.of(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage()));
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers, HttpStatusCode status,
+                                                                  WebRequest request) {
+        BindingResult bindingResult = ex.getBindingResult();
+        List<String> messages = new ArrayList<>();
+
+        for (FieldError fieldError : bindingResult.getFieldErrors()) {
+            messages.add(fieldError.getDefaultMessage() + " (" + fieldError.getRejectedValue() + ")");
+        }
+
+        log.error("MethodArgumentNotValidException : {}", messages, ex);
+
+        return ResponseEntity.badRequest()
+                .body(ErrorResponse.of(HttpStatus.BAD_REQUEST, messages));
     }
 }
--- src/main/java/com/project/goinghome/common/error/ErrorResponse.java
+++ src/main/java/com/project/goinghome/common/error/ErrorResponse.java
@@ -1,21 +1,29 @@
 package com.project.goinghome.common.error;
 
-import lombok.Builder;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
+import java.util.List;
+
 @Getter
-@Builder
-@RequiredArgsConstructor
 public class ErrorResponse {
     private final int code;
-    private final String message;
+    private final List<String> messages;
 
-    public static ErrorResponse of(HttpStatus status, String message) {
-        return ErrorResponse.builder()
-                .code(status.value())
-                .message(message)
-                .build();
+    public ErrorResponse(int code, List<String> messages) {
+        this.code = code;
+        this.messages = messages;
+    }
+
+    public ErrorResponse(int code, String... message) {
+        this(code, List.of(message));
+    }
+
+    public static ErrorResponse of(HttpStatus status, List<String> messages) {
+        return new ErrorResponse(status.value(), messages);
+    }
+
+    public static ErrorResponse of(HttpStatus status, String... message) {
+        return new ErrorResponse(status.value(), message);
     }
 }
